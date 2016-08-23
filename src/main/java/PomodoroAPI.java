c82cd0233eb534f04857653420949417cf6f5c81
prateeksultania <prateeksultania@gmail.com>
2016-08-23 21:27:12 +0530

    PomodoroServer: Add hack to handle activeUsers in case of uninstall install cycle

--- src/main/java/PomodoroAPI.java
+++ src/main/java/PomodoroAPI.java
@@ -64,9 +64,13 @@ public class PomodoroAPI {
     public static void addUser(PomodoroUser pomodoroUser) {
         String message = "New user installed app: " + pomodoroUser;
         System.out.println(message);
-        //hack to remove object in case the user installed and uninstalled the app
-        pomodoroUsers.remove(getPomodoroUser(pomodoroUser.getUserId()));
+        removeUninstalledUserIfExist(pomodoroUser.getUserId());
         pomodoroUsers.add(pomodoroUser);
+    }
+
+    private static void removeUninstalledUserIfExist(String userId) {
+        pomodoroUsers.remove(getPomodoroUser(userId));
+        activeUsersMap.remove(getPomodoroUser(userId));
     }
 
     public static PomodoroUser addActiveUser(String userId) {
